75fe48d0d79df57d826e2f61071f7a0d3f83d8dd
jeffersonrizzetto@gmail.com <jeffersonrizzetto@gmail.com>
2025-09-18 09:01:07 -0300

    Curso backend

---
+++ curso-completo-node.js-2025/dominando-node-js/index1.js
@@ -0,0 +1,9 @@
+let customers = [
+    {id: 1, name: "Jefferson", idade: 36},
+    {id: 2, name: "Pedro", idade: 30},
+    {id: 3, name: "Maria Lúcia", idade: 40}
+]
+
+const custumer = customers[customers.length - 1].id;
+
+console.log(custumer);
