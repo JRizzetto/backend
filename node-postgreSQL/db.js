d4fb552a5397660597ea5b000d24fd2dc81bbb13
jeffersonrizzetto@gmail.com <jeffersonrizzetto@gmail.com>
2025-09-24 16:34:25 -0300

    Finalizado projeto de postgreSQL

--- node-postgreSQL/db.js
+++ node-postgreSQL/db.js
@@ -19,8 +19,53 @@ async function connect() {
 
 connect();
 
+// Agora iremos fazer o CRUD
 // Listagem de clientes
 async function selectCustomers() {
-  // Estabelecer a conexão
+  // Estabelecer a conexão (pedir a conexão)
   const client = await connect();
+  // client.query() serve para enviar comando para o banco e passa o comando como atributo entre ""
+  const res = await client.query("SELECT * FROM clientes");
+  // Retornar as linhas retornadas como resposta
+  return res.rows;
 }
+
+// Listagem para pegar apenas 1 cliente
+async function selectCustomer(id) {
+  const client = await connect();
+  const res = await client.query("SELECT * FROM clientes WHERE ID=$1", [id]);
+  return res.rows;
+}
+
+// Inserir ou adicionar um cliente
+async function insertCustomer(customer) {
+  const client = await connect();
+  const sql = "INSERT INTO clientes(nome, idade, uf) VALUES($1, $2, $3)";
+  const values = [customer.nome, customer.idade, customer.uf];
+  await client.query(sql, values);
+}
+
+// Atualizar um cliente
+async function updateCustomer(id, customer) {
+  const client = await connect();
+  const sql = "UPDATE clientes SET nome=$1, idade=$2, uf=$3 WHERE id=$4";
+  const values = [customer.nome, customer.idade, customer.uf, id];
+  await client.query(sql, values);
+}
+
+// Deleter um cliente
+async function deleteCustomer(id) {
+  const client = await connect();
+  const sql = "DELETE FROM clientes WHERE id=$1";
+  const values = [id];
+  await client.query(sql, values);
+}
+
+// Exportar para poder usar no backend
+module.exports = {
+  selectCustomers,
+  selectCustomer,
+  insertCustomer,
+  updateCustomer,
+  deleteCustomer
+};
--- node-postgreSQL/index.js
+++ node-postgreSQL/index.js
@@ -8,10 +8,46 @@ const express = require("express");
 
 const app = express();
 
+// Configurar o ambiente para receber arquivos json (para a criação do post)
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.json({
     message: "funcionando",
   });
+});
+
+// Criar uma rota para retornar 1 cliente
+app.get("/clientes/:id", async (req, res) => {
+  const cliente = await db.selectCustomer(req.params.id);
+  res.json(cliente);
+});
+
+// Criar uma rota para listas nossos clientes
+app.get("/clientes", async (req, res) => {
+  const clientes = await db.selectCustomers();
+  res.json(clientes);
+});
+
+// Inserir ou adicionar um cliente
+app.post("/clientes", async (req, res) => {
+  await db.insertCustomer(req.body);
+  // O status de sucesso para cadastro é o 201
+  res.sendStatus(201);
+});
+
+// Atualizar um cliente
+app.patch("/clientes/:id", async (req, res) => {
+  await db.updateCustomer(req.params.id, req.body);
+  // O status de sucesso para atualizar é o 200
+  res.sendStatus(200);
+});
+
+// Deleter um cliente
+app.delete("/clientes/:id", async (req, res) => {
+  await db.deleteCustomer(req.params.id);
+  // O status de sucesso para excluir é o 204
+  res.sendStatus(204);
 });
 
 app.listen(port);
