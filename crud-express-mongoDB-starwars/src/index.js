0dc71988a19bcfcd1c2353ee94f45de542800f62
jeffersonrizzetto@gmail.com <jeffersonrizzetto@gmail.com>
2025-10-02 15:19:00 -0300

    Projeto finalizado, contrída a API com conexão ao mongoDB

--- crud-express-mongoDB-starwars/src/index.js
+++ crud-express-mongoDB-starwars/src/index.js
@@ -4,9 +4,6 @@ const mongoose = require("mongoose");
 const app = express();
 app.use(express.json());
 const port = 3000;
-mongoose.connect(
-  "mongodb+srv://jeffersonrizzetto_db_user:tfCk3hAjTockdHnK@crud-express-mongodb-st.8riuwj4.mongodb.net/?retryWrites=true&w=majority&appName=crud-express-mongoDB-starwars"
-);
 
 const Film = mongoose.model("Film", {
   title: String,
@@ -15,8 +12,30 @@ const Film = mongoose.model("Film", {
   trailer_url: String,
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get("/", async (req, res) => {
+  const films = await Film.find();
+  res.send(films);
+});
+
+app.delete("/:id", async (req, res) => {
+  const film = await Film.findByIdAndDelete(req.params.id);
+  return res.send(film);
+});
+
+app.put("/:id", async (req, res) => {
+  const film = await Film.findByIdAndUpdate(
+    req.params.id,
+    {
+      title: req.body.title,
+      description: req.body.description,
+      image_url: req.body.image_url,
+      trailer_url: req.body.trailer_url,
+    },
+    {
+      new: true,
+    }
+  );
+  return res.send(film);
 });
 
 app.post("/", async (req, res) => {
@@ -27,9 +46,12 @@ app.post("/", async (req, res) => {
     trailer_url: req.body.trailer_url,
   });
   await film.save();
-  res.send(film);
+  return res.send(film);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  mongoose.connect(
+    "mongodb+srv://jeffersonrizzetto_db_user:tfCk3hAjTockdHnK@crud-express-mongodb-st.8riuwj4.mongodb.net/?retryWrites=true&w=majority&appName=crud-express-mongoDB-starwars"
+  );
+  console.log(`Server running on port ${port}`);
 });
